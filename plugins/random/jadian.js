eede380bf287055ab9371d5e80b11abe7ec07370
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-12-05 12:57:20 +0700

    Update jadian.js

--- plugins/random/jadian.js
+++ plugins/random/jadian.js
@@ -4,15 +4,17 @@ exports.default = {
    command: ['jadian'],
    start: async (m, {
       conn,
-      participants
+      participants,
+      Format
    }) => {
       const user = participants.map(v => v.id);
-      const user_1 = pickRandom(user);
-      const user_2 = pickRandom(user);
+      const user_1 = pickRandom(user)
+      const user_2 = pickRandom(user)
       const mentions = [user_1, user_2]
-      conn.adReply(m.chat, `Jadian 😋\n@${user_1.split('@')[0]}  ❤️ @${user_1.split('@')[0]}`, cover, m, {
+      console.log(mentions)
+      conn.adReply(m.chat, `Jadian 😋\n@${user_1.split('@')[0]}  ❤️ @${user_2.split('@')[0]}`, cover, m, {
          mentions: mentions,
-         showAds: true
+         showAds: false
       })
    },
    group: true

29444b3ece27ef687e6fc867a91d19d2917888b5
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-12-07 17:38:32 +0700

    Update info-status-bot.js

--- plugins/info/info-status-bot.js
+++ plugins/info/info-status-bot.js
@@ -12,6 +12,7 @@ exports.default = {
       caption += `Auto Backup DB: ${global.auto_backup ? y : t}\n`
       caption += `Auto Status Bio: ${db.settings.autobio ? y : t}\n`
       caption += `Auto Read Story: ${db.settings.readsw ? y : t}\n`      
+      caption += `Anti Call: ${global.anticall ? y : t}\n`
       caption += `Group Mode: ${global.group_mode ? y : t}\n`
       caption += `Mess Group Only: ${global.group_only_message ? y : t}\n`
       caption += `AdReply: ${global.adReply ? y : t}\n`
@@ -25,7 +26,7 @@ exports.default = {
       caption += `Recording Private: ${global.recording_private ? y : t}\n`
       caption += `Ram Set: ${setting.ram}\n`
       caption += `Prefix: ${db.settings.prefix}\n\n`
-      caption += `Untuk mengubah pengaturan langsung dari bot owner bisa cek di menu .set`      
+      caption += `Untuk mengubah pengaturan langsung dari bot owner bisa cek di menu .set atau ada juga di menu .on .off`      
       conn.reply(m.chat, caption, m)
    }
 }
